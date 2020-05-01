e83ea87b99224d94553a890717c125af95015fbe
Andrew Green <green.andrew.b@gmail.com>
2020-05-01 15:47:39 -0500

    part 4 done

--- cohort.py
+++ cohort.py
@@ -1,5 +1,8 @@
 class Cohort:
-    def __init__(self, name):
+    def __init__(self, id, name):
         self.name = name
-        self.students = list()
-        self.instructors = list()
+        # self.students = list()
+        # self.instructors = list()
+
+    def __repr__(self):
+        return f'{self.name}'
--- student.py
+++ student.py
@@ -1,9 +1,13 @@
 from nss_person import NSSPerson
 
 class Student(NSSPerson):
-    def __init__(self, first, last, slack, cohort):
+
+    def __init__(self, id, first, last, handle, cohort):
+        self.id = id
         self.first_name = first
         self.last_name = last
-        self.slack_handle = slack
+        self.slack_handle = handle
         self.cohort = cohort
-        self.exercises = list()
+
+    def __repr__(self):
+        return f'{self.first_name} {self.last_name} is in {self.cohort}'
